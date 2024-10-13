4b409f5996cfaafaa4a8412ae1742f99b3185fce
Czar <hz.czar@sjtu.edu.cn>
2024-10-14 00:50:48 +0800

    fix

--- src/main/java/Compiler/Src/OPT/LiveAnalysis.java
+++ src/main/java/Compiler/Src/OPT/LiveAnalysis.java
@@ -40,9 +40,9 @@ import Compiler.Src.Util.ScopeUtil.GlobalScope;
 public class LiveAnalysis {
 
     HashMap<IRVariable, IRBlock> Var2Def;
-    HashMap<IRVariable, HashSet<IRBlock>> Var2Use;
+    HashMap<IRVariable, HashSet<Pair<IRBlock, Pair<IRInst,Boolean>>>> Var2Use;
     HashSet<IRVariable> phidef;
-    HashSet<Pair<IRBlock,IRVariable>> phiuse;
+    HashSet<Pair<IRBlock, IRVariable>> phiuse;
     IRFuncDef curFunc;
     // int number=0;
 
@@ -53,32 +53,32 @@ public class LiveAnalysis {
 
     public void work_on_func(IRFuncDef func) {
         HashMap<IRBlock, HashSet<IRVariable>> OldOut = new HashMap<IRBlock, HashSet<IRVariable>>();
-        for(var block:func.getBlockstmts()){
-            OldOut.put(block,new HashSet<IRVariable>(block.getLiveOut()));
+        for (var block : func.getBlockstmts()) {
+            OldOut.put(block, new HashSet<IRVariable>(block.getLiveOut()));
         }
         Var2Def = new HashMap<IRVariable, IRBlock>();
-        Var2Use = new HashMap<IRVariable, HashSet<IRBlock>>();
+        Var2Use = new HashMap<IRVariable, HashSet<Pair<IRBlock, Pair<IRInst,Boolean>>>>();
         phidef = new HashSet<IRVariable>();
-        phiuse = new HashSet<Pair<IRBlock,IRVariable>>();
+        phiuse = new HashSet<Pair<IRBlock, IRVariable>>();
         curFunc = func;
         Collect(func);
         // 要特判PHI的def
         work();
-        for(var block:func.getBlockstmts()){
-            for(var var:OldOut.get(block)){
-                if(!block.getLiveOut().contains(var) && !var.isGlobal()){
-                    // throw new OPTError("LiveAnalysis Error");
-                    int a=1;
-                }
-            }
-        }
-        for(var block:func.getBlockstmts()){
-            for(var var:block.getLiveOut()){
-                if(!OldOut.get(block).contains(var)){
-                    int a=1;
-                }
-            }
-        }
+        // for (var block : func.getBlockstmts()) {
+        //     for (var var : OldOut.get(block)) {
+        //         if (!block.getLiveOut().contains(var) && !var.isGlobal()) {
+        //             // throw new OPTError("LiveAnalysis Error");
+        //             int a = 1;
+        //         }
+        //     }
+        // }
+        // for (var block : func.getBlockstmts()) {
+        //     for (var var : block.getLiveOut()) {
+        //         if (!OldOut.get(block).contains(var)) {
+        //             int a = 1;
+        //         }
+        //     }
+        // }
     }
 
     public void Collect(IRFuncDef func) {
@@ -109,51 +109,50 @@ public class LiveAnalysis {
                     }
                     if (pred.getReturnInst() instanceof IRBranch && !((IRBranch) pred.getReturnInst()).isJump()) {
                         if (Var2Use.containsKey(val)) {
-                            Var2Use.get(val).add(block);
+                            Var2Use.get(val).add(new Pair<IRBlock, Pair<IRInst,Boolean>>(block, new Pair<IRInst, Boolean>(phi, false)));
                         } else {
-                            Var2Use.put(val, new HashSet<IRBlock>());
-                            Var2Use.get(val).add(block);
+                            Var2Use.put(val, new HashSet<Pair<IRBlock, Pair<IRInst,Boolean>>>());
+                            Var2Use.get(val).add(new Pair<IRBlock, Pair<IRInst,Boolean>>(block, new Pair<IRInst, Boolean>(phi, false)));
                         }
-                        phiuse.add(new Pair<IRBlock,IRVariable>(block,val));
+                        phiuse.add(new Pair<IRBlock, IRVariable>(block, val));
                     } else {
                         if (Var2Use.containsKey(val)) {
-                            Var2Use.get(val).add(pred);
+                            Var2Use.get(val).add(new Pair<IRBlock, Pair<IRInst,Boolean>>(pred, new Pair<IRInst, Boolean>(phi, true)));
                         } else {
-                            Var2Use.put(val, new HashSet<IRBlock>());
-                            Var2Use.get(val).add(pred);
+                            Var2Use.put(val, new HashSet<Pair<IRBlock, Pair<IRInst,Boolean>>>());
+                            Var2Use.get(val).add(new Pair<IRBlock, Pair<IRInst,Boolean>>(pred, new Pair<IRInst, Boolean>(phi, true)));
                         }
                     }
                 }
             }
             for (var inst : block.getInsts()) {
-                if(inst.getDef()!=null)
-                {
+                if (inst.getDef() != null) {
                     Var2Def.put(inst.getDef(), block);
                 }
                 for (var var : inst.getUses()) {
                     if (Var2Use.containsKey(var)) {
-                        Var2Use.get(var).add(block);
+                        Var2Use.get(var).add(new Pair<IRBlock, Pair<IRInst,Boolean>>(block, new Pair<IRInst, Boolean>(inst, false)));
                     } else {
-                        Var2Use.put(var, new HashSet<IRBlock>());
-                        Var2Use.get(var).add(block);
+                        Var2Use.put(var, new HashSet<Pair<IRBlock, Pair<IRInst,Boolean>>>());
+                        Var2Use.get(var).add(new Pair<IRBlock, Pair<IRInst,Boolean>>(block, new Pair<IRInst, Boolean>(inst, false)));
                     }
                 }
             }
             for (var var : block.getReturnInst().getUses()) {
                 if (Var2Use.containsKey(var)) {
-                    Var2Use.get(var).add(block);
+                    Var2Use.get(var).add(new Pair<IRBlock, Pair<IRInst,Boolean>>(block, new Pair<IRInst, Boolean>(block.getReturnInst(), false)));
                 } else {
-                    Var2Use.put(var, new HashSet<IRBlock>());
-                    Var2Use.get(var).add(block);
+                    Var2Use.put(var, new HashSet<Pair<IRBlock, Pair<IRInst,Boolean>>>());
+                    Var2Use.get(var).add(new Pair<IRBlock, Pair<IRInst,Boolean>>(block, new Pair<IRInst, Boolean>(block.getReturnInst(), false)));
                 }
             }
         }
         for (var param : func.getParams()) {
             // if (Var2Use.containsKey(param)) {
-            //     Var2Use.get(param).add(func.getBlockstmts().get(0));
+            // Var2Use.get(param).add(func.getBlockstmts().get(0));
             // } else {
-            //     Var2Use.put(param, new HashSet<IRBlock>());
-            //     Var2Use.get(param).add(func.getBlockstmts().get(0));
+            // Var2Use.put(param, new HashSet<IRBlock>());
+            // Var2Use.get(param).add(func.getBlockstmts().get(0));
             // }
             Var2Def.put(param, func.getBlockstmts().get(0));
         }
@@ -164,21 +163,37 @@ public class LiveAnalysis {
             IRBlock def = Var2Def.get(var);
             HashSet<IRBlock> WorkList = new HashSet<IRBlock>();
             HashSet<IRBlock> Visited = new HashSet<IRBlock>();
-            if(!Var2Use.containsKey(var))
-            {
+            if (var.getValue().equals("%.tmp.binary.33")) {
+                int a = 1;
+            }
+            if (!Var2Use.containsKey(var)) {
                 continue;
             }
-            for (var block : Var2Use.get(var)) {
+            for (var pair : Var2Use.get(var)) {
+                var block = pair.a;
                 if (block == def) {
-                    if(phiuse.contains(new Pair<IRBlock,IRVariable>(block,var))){
+                    if (phiuse.contains(new Pair<IRBlock, IRVariable>(block, var))) {
                         WorkList.add(block);
                         Visited.add(block);
                     }
                     continue;
                 }
                 for (var pred : block.getPredecessors()) {
-                    WorkList.add(pred);
-                    Visited.add(pred);
+                    if (pair.b.a instanceof IRPhi && pair.b.b == false) {
+                        IRPhi phi = (IRPhi) pair.b.a;
+                        for (int i = 0; i < phi.getVals().size(); ++i) {
+                            if (phi.getVals().get(i).equals(var)) {
+                                if (phi.getLabels().get(i).equals(pred.getLabelName())) {
+                                    WorkList.add(pred);
+                                    Visited.add(pred);
+                                    break;
+                                }
+                            }
+                        }
+                    } else {
+                        WorkList.add(pred);
+                        Visited.add(pred);
+                    }
                 }
             }
             while (!WorkList.isEmpty()) {
@@ -195,9 +210,10 @@ public class LiveAnalysis {
                     }
                 }
             }
-            if(phidef.contains(var)){
-                for(var pred:def.getPredecessors()){
-                    if(!Visited.contains(pred) && pred.getReturnInst() instanceof IRBranch && ((IRBranch)pred.getReturnInst()).isJump()){
+            if (phidef.contains(var)) {
+                for (var pred : def.getPredecessors()) {
+                    if (!Visited.contains(pred) && pred.getReturnInst() instanceof IRBranch
+                            && ((IRBranch) pred.getReturnInst()).isJump()) {
                         pred.getLiveOut().add(var);
                     }
                 }
@@ -205,7 +221,7 @@ public class LiveAnalysis {
         }
         // var entry=curFunc.getBlockstmts().get(0);
         // for(var param:curFunc.getParams()){
-        //     entry.getLiveOut().add(param);
+        // entry.getLiveOut().add(param);
         // }
     }
 }
