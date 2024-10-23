f8064eafb41c06217cd072164a885fbeb559868b
Czar <hz.czar@sjtu.edu.cn>
2024-10-23 18:26:11 +0800

    SCCP_ADCE

--- src/main/java/Compiler/Src/OPT/IROptimize.java
+++ src/main/java/Compiler/Src/OPT/IROptimize.java
@@ -5,16 +5,15 @@ import Compiler.Src.IR.Node.IRRoot;
 public class IROptimize {
     public void visit(IRRoot root)
     {
-        new LoadStoreOpt().visit(root);
+        // new LoadStoreOpt().visit(root);
         new Mem2Reg().visit(root);
-        new IROther().visit(root);
-        new LoopOpt().visit(root);
-        new Tail().visit(root);
-        new Inlining().visit(root);
-        // new ADCE().visit(root);
+        // new IROther().visit(root);
+        // new LoopOpt().visit(root);
+        // new Tail().visit(root);
+        // new Inlining().visit(root);
         new SCCP().visit(root);
         new ADCE().visit(root);
-        new CSE().visit(root);
+        // new CSE().visit(root);
         // new RovB().visit(root);
         new LiveAnalysis().visit(root);
         return;
